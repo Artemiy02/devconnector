59b2c79d7fe315fdfb62a555ba821fccda273974
Artem Obzhelyansliy <artemiy02@yandex.ru>
2019-03-26 12:12:32 +0200

    add like route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -69,6 +69,7 @@ router.delete(
   '/:post_id',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
       Post.findById(req.params.post_id)
         .then(post => {
           // Check for post owner
@@ -80,6 +81,30 @@ router.delete(
           post.remove().then(() => res.json({ success: true }));
         })
         .catch(err => res.status(404).json({ postnotfoud: 'Post not found' }));
+    });
+});
+
+// @route  POST api/posts/like/:id
+// @desc   Like post
+// @access Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ alreadyliked: 'User already liked this post' });
+          }
+
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfoud: 'Post not found' }));
+    });
 });
 
 
