d1ca34265555c947a09c8d9f3ecd3ab2cf3469f5
Artem Obzhelyansliy <artemiy02@yandex.ru>
2019-05-01 22:58:32 +0300

    Prepared for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null;
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    devTools
+    applyMiddleware(...middleware)
   )
 );
 
